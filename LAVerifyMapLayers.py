1ae189044f77995ffa47a6b4d1ee8aba48a6a5e1
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-25 14:34:27 -0700

    Excel Update

--- LAVerifyMapLayers.py
+++ LAVerifyMapLayers.py
@@ -88,43 +88,43 @@ class Verify_Idaho_Layers(unittest.TestCase):
         driver.close()
         print 'Chrome'
 
-    def test_presence_of_correct_layers_FIREFOX(self):
-        print
-        self.driver = webdriver.Firefox()
-        driver = self.driver
-        driver.get(url)
-        driver.maximize_window()
-
-        dropDownMenuWait = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, 'layers-menu-dropdown-button')))
-        driver.find_element_by_id('layers-menu-dropdown-button').click()
-
-        itemWait = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="layerSelector"]/ul/li[1]/a/span/img[1]')))
-        # 1. First Item Verification: LA Road Reports
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[0], itemText[0], itemLink[0]), "LA Road Reports Is Faulty"
-        # 2. Second Item Verification: LA Construction
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[1], itemText[1], itemLink[1]), "LA Construction Is Faulty"
-        # 3. Third Item Verification: LA Waze Reports
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[2], itemText[2], itemLink[2]), "LA Waze Reports Is Faulty"
-        # 4. Fourth Item Verification: Weather Warnings
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[3], itemText[3],
-                                           itemLink[3]), "LA Weather Warnings Is Faulty"
-        # 5. Fifth Item Verification: Ferry Status
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[4], itemText[4], itemLink[4]), "LA Ferry Status Is Faulty"
-        # 6. Sixth Item Verification: Flooding/Ice
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[5], itemText[5], itemLink[5]), "LA Flooding/Ice Is Faulty"
-        # 7. Seventh Item Verification: Cameras
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[6], itemText[6], itemLink[6]), "LA Cameras Are Faulty"
-        # 8. Eighth Item Verification: Google Speeds and Incidents
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[7], itemText[7],
-                                           itemLink[7]), "Google Speeds and Incidents Are Faulty"
-        # 9. Ninth Item Verification: Other States' Info
-        assert Verify_Layer_Drop_Down_Item(driver, itemXpath[8], itemText[8],
-                                           itemLink[8]), "Other States' Info Are Faulty"
-
-        driver.close()
-        print 'Firefox'
+    # def test_presence_of_correct_layers_FIREFOX(self):
+    #     print
+    #     self.driver = webdriver.Firefox()
+    #     driver = self.driver
+    #     driver.get(url)
+    #     driver.maximize_window()
+    #
+    #     dropDownMenuWait = WebDriverWait(driver, 20).until(
+    #         EC.presence_of_element_located((By.ID, 'layers-menu-dropdown-button')))
+    #     driver.find_element_by_id('layers-menu-dropdown-button').click()
+    #
+    #     itemWait = WebDriverWait(driver, 20).until(
+    #         EC.presence_of_element_located((By.XPATH, '//*[@id="layerSelector"]/ul/li[1]/a/span/img[1]')))
+    #     # 1. First Item Verification: LA Road Reports
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[0], itemText[0], itemLink[0]), "LA Road Reports Is Faulty"
+    #     # 2. Second Item Verification: LA Construction
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[1], itemText[1], itemLink[1]), "LA Construction Is Faulty"
+    #     # 3. Third Item Verification: LA Waze Reports
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[2], itemText[2], itemLink[2]), "LA Waze Reports Is Faulty"
+    #     # 4. Fourth Item Verification: Weather Warnings
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[3], itemText[3],
+    #                                        itemLink[3]), "LA Weather Warnings Is Faulty"
+    #     # 5. Fifth Item Verification: Ferry Status
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[4], itemText[4], itemLink[4]), "LA Ferry Status Is Faulty"
+    #     # 6. Sixth Item Verification: Flooding/Ice
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[5], itemText[5], itemLink[5]), "LA Flooding/Ice Is Faulty"
+    #     # 7. Seventh Item Verification: Cameras
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[6], itemText[6], itemLink[6]), "LA Cameras Are Faulty"
+    #     # 8. Eighth Item Verification: Google Speeds and Incidents
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[7], itemText[7],
+    #                                        itemLink[7]), "Google Speeds and Incidents Are Faulty"
+    #     # 9. Ninth Item Verification: Other States' Info
+    #     assert Verify_Layer_Drop_Down_Item(driver, itemXpath[8], itemText[8],
+    #                                        itemLink[8]), "Other States' Info Are Faulty"
+    #
+    #     driver.close()
+    #     print 'Firefox'
 
 if __name__ == '__main__':
     print ('\n') + "Verifying TG Web Map Layers" + '\n'
