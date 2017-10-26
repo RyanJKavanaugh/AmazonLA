89a2949a09eaa791511e20df789207696be04db3
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-26 10:30:44 -0700

    Update

--- LAVerifyUserLogin.py
+++ LAVerifyUserLogin.py
@@ -28,8 +28,8 @@ username = worksheet.cell(1, 1).value
 password = worksheet.cell(1, 2).value
 adjustResolution = worksheet.cell(1, 3).value
 
-# if adjustResolution == 1:
-#     AdjustResolution()
+if adjustResolution == 1:
+    AdjustResolution()
 
 
 class Verify_Login_And_Saving_Routes(unittest.TestCase):
