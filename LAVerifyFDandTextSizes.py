c4c2e37aa8c8b90d5fbe52433a98528dd76326e4
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-26 09:20:58 -0700

    Update

--- LAVerifyFDandTextSizes.py
+++ LAVerifyFDandTextSizes.py
@@ -55,27 +55,27 @@ class Verify_Idaho_Links(unittest.TestCase):
         driver.close()
 
 
-    def test_Future_Info_Toolbar_Is_Active_Firefox(self):
-
-        self.driver = webdriver.Firefox()
-        driver = self.driver
-        driver.set_window_size(1800, 1100)
-        driver.get(url)
-        loginElement = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'timeFrameSelectorDiv')))
-
-        driver.find_element_by_id('timeFrameSelectorDiv').click()
-
-        assert driver.find_element_by_id('timeFrameSelectorDiv').is_enabled()
-
-        assert driver.find_element_by_id('smallTextLnk').is_enabled()
-
-        assert driver.find_element_by_id('normalTextLnk').is_enabled()
-
-        assert driver.find_element_by_id('largeTextLnk').is_enabled()
-
-        assert driver.find_element_by_id('textOnlySiteLinkSpan').is_enabled()
-
-        driver.close()
+    # def test_Future_Info_Toolbar_Is_Active_Firefox(self):
+    #
+    #     self.driver = webdriver.Firefox()
+    #     driver = self.driver
+    #     driver.set_window_size(1800, 1100)
+    #     driver.get(url)
+    #     loginElement = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'timeFrameSelectorDiv')))
+    #
+    #     driver.find_element_by_id('timeFrameSelectorDiv').click()
+    #
+    #     assert driver.find_element_by_id('timeFrameSelectorDiv').is_enabled()
+    #
+    #     assert driver.find_element_by_id('smallTextLnk').is_enabled()
+    #
+    #     assert driver.find_element_by_id('normalTextLnk').is_enabled()
+    #
+    #     assert driver.find_element_by_id('largeTextLnk').is_enabled()
+    #
+    #     assert driver.find_element_by_id('textOnlySiteLinkSpan').is_enabled()
+    #
+    #     driver.close()
 
 
 if __name__ == '__main__':
